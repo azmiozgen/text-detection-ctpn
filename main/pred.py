b46848aabada1c6b17eee293595ce84b6898d114
Azmi Ozgen <ozgenaz@itu.edu.tr>
2019-08-09 17:12:26 +0300

    Go back to backup

--- main/pred.py
+++ main/pred.py
@@ -76,9 +76,7 @@ def main(argv=None):
             model_path = os.path.join(FLAGS.checkpoint_dir, os.path.basename(ckpt_state.model_checkpoint_path))
             print('Restore from {}'.format(model_path))
             saver.restore(sess, model_path)
-
-            images = []
-            im_infos = []
+            
             im_fn_list = get_images()
             for im_fn in im_fn_list:
                 print('===============')
@@ -87,53 +85,59 @@ def main(argv=None):
                 if os.path.isfile(output_file):
                     print("{} exists".format(output_file))
                     continue
+
                 start = time.time()
                 try:
                     im = cv2.imread(im_fn)[:, :, ::-1]
                 except:
                     print("Error reading image {}!".format(im_fn))
+                    cost_time = (time.time() - start)
+                    print("cost time: {:.2f}s".format(cost_time))
+                    continue
+
+                ## TODO threshold and check if image is empty
+                _, thresh = cv2.threshold(cv2.cvtColor(im, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+                if len(np.unique(thresh)) == 1:
+                    print("Image is empty. Passing")
+                    cost_time = (time.time() - start)
+                    print("cost time: {:.2f}s".format(cost_time))
                     continue
 
                 img, (rh, rw) = resize_image(im)
                 h, w, c = img.shape
-                # print("IMAGE SHAPE:", img.shape)
-                images.append(img)
+                print("IMAGE SHAPE:", img.shape)
                 im_info = np.array([h, w, c]).reshape([1, 3])
-                im_infos.append(im_info)
+                bbox_pred_val, cls_prob_val = sess.run([bbox_pred, cls_prob],
+                                                       feed_dict={input_image: [img],
+                                                                  input_im_info: im_info})
 
-            images = np.array(images).reshape(len(images), h, w, c)
-            im_infos = np.array(im_infos).reshape(len(im_infos), 3)
-            bbox_pred_val, cls_prob_val = sess.run([bbox_pred, cls_prob],
-                                                    feed_dict={input_image: images,
-                                                                input_im_info: im_infos})
+                textsegs, _ = proposal_layer(cls_prob_val, bbox_pred_val, im_info)
+                scores = textsegs[:, 0]
+                textsegs = textsegs[:, 1:5]
 
-            textsegs, _ = proposal_layer(cls_prob_val, bbox_pred_val, im_info)
-            scores = textsegs[:, 0]
-            textsegs = textsegs[:, 1:5]
+                textdetector = TextDetector(DETECT_MODE='O')
+                boxes = textdetector.detect(textsegs, scores[:, np.newaxis], img.shape[:2])
+                boxes = np.array(boxes, dtype=np.int)
 
-            textdetector = TextDetector(DETECT_MODE='O')
-            boxes = textdetector.detect(textsegs, scores[:, np.newaxis], img.shape[:2])
-            boxes = np.array(boxes, dtype=np.int)
+                cost_time = (time.time() - start)
+                print("cost time: {:.2f}s".format(cost_time))
 
-            cost_time = (time.time() - start)
-            print("cost time: {:.2f}s".format(cost_time))
+                # for i, box in enumerate(boxes):
+                #     cv2.polylines(img, [box[:8].astype(np.int32).reshape((-1, 1, 2))], True, color=(0, 255, 0),
+                #                   thickness=2)
+                # img = cv2.resize(img, None, None, fx=1.0 / rh, fy=1.0 / rw, interpolation=cv2.INTER_LINEAR)
+                # cv2.imwrite(os.path.join(FLAGS.output_dir, os.path.basename(im_fn)), img[:, :, ::-1])
 
-            # for i, box in enumerate(boxes):
-            #     cv2.polylines(img, [box[:8].astype(np.int32).reshape((-1, 1, 2))], True, color=(0, 255, 0),
-            #                   thickness=2)
-            # img = cv2.resize(img, None, None, fx=1.0 / rh, fy=1.0 / rw, interpolation=cv2.INTER_LINEAR)
-            # cv2.imwrite(os.path.join(FLAGS.output_dir, os.path.basename(im_fn)), img[:, :, ::-1])
+                boxes = boxes.astype(np.float)
+                boxes[:, [0, 2, 4, 6]] *= 1.0 / rw
+                boxes[:, [1, 3, 5, 7]] *= 1.0 / rh
+                boxes = boxes.astype(np.int)
 
-            boxes = boxes.astype(np.float)
-            boxes[:, [0, 2, 4, 6]] *= 1.0 / rw
-            boxes[:, [1, 3, 5, 7]] *= 1.0 / rh
-            boxes = boxes.astype(np.int)
-
-            with open(output_file, 'w') as f:
-                for i, box in enumerate(boxes):
-                    line = ",".join(str(box[k]) for k in range(8))
-                    line += "," + str(scores[i]) + "\r\n"
-                    f.writelines(line)
+                with open(output_file, 'w') as f:
+                    for i, box in enumerate(boxes):
+                        line = ",".join(str(box[k]) for k in range(8))
+                        line += "," + str(scores[i]) + "\r\n"
+                        f.writelines(line)
 
 if __name__ == '__main__':
     tf.app.run()
